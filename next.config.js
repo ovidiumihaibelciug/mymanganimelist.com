58d3e8fffc306052646e24b29489e396c707728a
ovidiumihaibelciug <ovidiumihaibelciug@gmail.com>
2019-05-17 19:29:43 +0300

    now deploy works

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 const withSass = require('@zeit/next-sass')
 
-module.exports = withSass();
-
-// module.exports = {
-//   webpack: config => {
-//     // Fixes npm packages that depend on `fs` module
-//     config.node = {
-//       fs: 'empty'
-//     }
-//
-//     return config
-//   }
-// }
+module.exports = withSass({
+  target: "serverless"
+});
