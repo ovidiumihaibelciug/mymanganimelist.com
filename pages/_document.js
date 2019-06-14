4ae2d1a2df4bbb256012fc208be850a9713e9bd2
Dacian Spinu <dacian@sourcetask.com>
2019-06-14 10:10:23 +0300

    added popads code

--- pages/_document.js
+++ pages/_document.js
@@ -28,11 +28,45 @@ export default class extends Document {
           `
             }}
           />
+          <!-- PopAds.net Popunder Code for mymanganimelist.com -->
+          <
+              type="text/javascript"
+              data-cfasync="false"
+              dangerouslySetInnerHTML={{ __html: `
+                    /*<![CDATA[/* */
+                    var _pop = _pop || [];
+                    _pop.push(['siteId', 3405187]);
+                    _pop.push(['minBid', 0]);
+                    _pop.push(['popundersPerIP', 0]);
+                    _pop.push(['delayBetween', 0]);
+                    _pop.push(['default', false]);
+                    _pop.push(['defaultPerDay', 0]);
+                    _pop.push(['topmostLayer', false]);
+                    (function() {
+                    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
+                    var s = document.getElementsByTagName('script')[0];
+                    pa.src = '//c1.popads.net/pop.js';
+                    pa.onerror = function() {
+                    var sa = document.createElement('script'); sa.type = 'text/javascript'; sa.async = true;
+                    sa.src = '//c2.popads.net/pop.js';
+                    s.parentNode.insertBefore(sa, s);
+                  };
+                    s.parentNode.insertBefore(pa, s);
+                  })();
+                  /*]]>/* */
+              
+              ` }}
+          />
+          <!-- PopAds.net Popunder Code End -->
           <script
             async
             src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
           />
-          <script
+          <script type="text/javascript" src="//deloplen.com/apu.php?zoneid=2663999" async data-cfasync="false"></script>
+            <script src="//pushlinck.com/ntfc.php?p=2509555" data-cfasync="false" async></script>
+            <script type="text/javascript" src="//deloplen.com/apu.php?zoneid=2509470" async data-cfasync="false"></script>
+            <script src="//pushmono.com/ntfc.php?p=2664061" data-cfasync="false" async></script>
+            <script
             dangerouslySetInnerHTML={{
               __html: `
                 (adsbygoogle = window.adsbygoogle || []).push({
